d1afc704598b538ec12b3c80855ad244d4605c0c
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-09-05 16:03:28 -0300

    up

---
+++ src/main/java/br/com/horariodobusao/ProjetoBusao/model/Pessoa.java
@@ -0,0 +1,8 @@
+package br.com.horariodobusao.ProjetoBusao.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+}
