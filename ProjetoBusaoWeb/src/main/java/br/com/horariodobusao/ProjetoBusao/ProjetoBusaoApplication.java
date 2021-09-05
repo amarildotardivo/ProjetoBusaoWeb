87739886c81c973213fc139c4bc92cebdf921ad9
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-09-05 14:12:49 -0300

    Criação do Repositorio

---
+++ ProjetoBusaoWeb/src/main/java/br/com/horariodobusao/ProjetoBusao/ProjetoBusaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.horariodobusao.ProjetoBusao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBusaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBusaoApplication.class, args);
+	}
+
+}
