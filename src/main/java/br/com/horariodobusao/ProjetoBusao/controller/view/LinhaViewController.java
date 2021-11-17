b4bbe03199d263f6e30b235de5189d810af1f56c
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-11-16 22:49:43 -0300

    up

--- src/main/java/br/com/horariodobusao/ProjetoBusao/controller/view/LinhaViewController.java
+++ src/main/java/br/com/horariodobusao/ProjetoBusao/controller/view/LinhaViewController.java
@@ -2,9 +2,11 @@ package br.com.horariodobusao.ProjetoBusao.controller.view;
 
 import br.com.horariodobusao.ProjetoBusao.model.*;
 import br.com.horariodobusao.ProjetoBusao.service.*;
+import javax.validation.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
+import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -26,5 +28,62 @@ public class LinhaViewController {
         return "formLinha";
     }
     
+    @PostMapping(path="/linha")
+    public String save(@Valid @ModelAttribute Linha linha, 
+            BindingResult result,
+            Model model){
+        
+        if(result.hasErrors()){
+            model.addAttribute("msgErros", result.getAllErrors());
+            return "formFuncionario";
+        }
+        
+        linha.setId(null);
+        
+        try{
+            service.save(linha);
+            model.addAttribute("msgSucesso", "Linha cadastrada com sucesso!");
+            model.addAttribute("linha", new Linha());
+            return "formLinha";
+        }catch(Exception e){
+            model.addAttribute("msgErros", new ObjectError("linha", e.getMessage()));
+            return "formLinha";
+        }
+    }
     
+    @GetMapping(path="/linha/{id}")
+    public String atualizacao(@PathVariable("id") Long id, Model model){
+        model.addAttribute("linha", service.findById(id));
+        return "formLinha";
+    }
+    
+    @PostMapping(path="/linha/{id}")
+    public String update(@Valid @ModelAttribute Linha linha, 
+            BindingResult result,
+            @PathVariable("id") Long id,
+            Model model){
+        
+        if(result.hasErrors()){
+            model.addAttribute("msgErros", result.getAllErrors());
+            return "formLinha";
+        }
+        
+        linha.setId(id);
+        
+        try{
+            service.update(linha);
+            model.addAttribute("msgSucesso", "Cidade atualizada com sucesso!");
+            model.addAttribute("linha", linha);
+            return "formLinha";
+        }catch(Exception e){
+            model.addAttribute("msgErros", new ObjectError("linha", e.getMessage()));
+            return "formLinha";
+        }
+    }
+    
+    @GetMapping(path="/{id}/deletar")
+    public String deletar(@PathVariable("id") Long id){
+        service.delete(id);
+        return "redirect:/linhas";
+    }
 }
