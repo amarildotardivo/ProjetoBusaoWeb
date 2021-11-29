22b7b5e18b9b70567d951caee70668a06592cece
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-11-29 20:58:47 -0300

    Create PessoaDetailsService.java

---
+++ src/main/java/br/com/horariodobusao/ProjetoBusao/security/PessoaDetailsService.java
@@ -0,0 +1,8 @@
+package br.com.horariodobusao.ProjetoBusao.security;
+
+import org.springframework.stereotype.*;
+
+@Service
+public class PessoaDetailsService {
+    //Farei a implementação posteriormente, devido a falta de tempo hábil
+}
